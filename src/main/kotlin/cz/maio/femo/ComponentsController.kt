1f2b7cfd6dfe52b76b8831fc1bf6c1bd51f8d335
Marian Schubert <marian.schubert@gmail.com>
2023-10-20 19:51:53 +0200

    Improve components using layout dialect

--- src/main/kotlin/cz/maio/femo/ComponentsController.kt
+++ src/main/kotlin/cz/maio/femo/ComponentsController.kt
@@ -1,10 +1,23 @@
 package cz.maio.femo
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.ModelAndView
 
 @Controller
-class ComponentsController {
+class ComponentsController(
+    private val objectMapper: ObjectMapper
+) {
     @GetMapping("/components")
-    fun components() = "components/components"
+    fun components(): ModelAndView {
+        val users = objectMapper.readTree(
+            ClassPathResource("/data/users.json").inputStream
+        )
+
+        return ModelAndView("components/components").apply {
+            addObject("users", users)
+        }
+    }
 }
