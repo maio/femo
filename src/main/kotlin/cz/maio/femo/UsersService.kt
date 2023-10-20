bbdd2d3402f24ca3e3cbcd9daabe3969d81bb38b
Marian Schubert <marian.schubert@gmail.com>
2023-10-20 21:01:49 +0200

    Various fixes and improvements

--- src/main/kotlin/cz/maio/femo/UsersService.kt
+++ src/main/kotlin/cz/maio/femo/UsersService.kt
@@ -13,7 +13,8 @@ class UsersService(
 
     data class User(
         val name: String,
-        val email: String
+        val username: String,
+        val email: String,
     )
 
     fun getUsers(): List<User> = objectMapper.readValue<List<User>>(usersJson.inputStream)
